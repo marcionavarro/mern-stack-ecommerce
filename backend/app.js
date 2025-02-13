8c0637acdc0ebea61a290bdb256b660df622d202
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-13 14:21:26 -0300

    #7 Conectando o site ao MongoDB para gerenciamento...

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-import express from 'express';
-import product from './routes/ProductRoutes.js';
+import express from "express";
+import product from "./routes/ProductRoutes.js";
 
 const app = express();
 
 //Route
-app.use("/api/v1", product)
+app.use("/api/v1", product);
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import { connectMongoDatabase } from "./config/db.js";
 
 dotenv.config({path:'backend/config/config.env'});
+
+connectMongoDatabase()
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
