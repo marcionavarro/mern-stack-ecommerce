f0daba66454be4f934c7f9ae0a733bed89cbeb0d
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-13 15:15:35 -0300

    #10 Obter todos os produtos

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,14 +1,18 @@
-import Product from './../models/productModel.js';
+import Product from "./../models/productModel.js";
 
 // Creating Products
 export const createProducts = async (req, res) => {
-    const product = await Product.create(req.body)
-    res.status(201).json({
-        success: true,
-        product
-    })
-}
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
 
-export const getAllProducts = (req, res) => {
-    res.status(200).json({message: 'All Products'})
-}
+export const getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
