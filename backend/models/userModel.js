659c87ee7aa2468eda498e11ddc5f9a37e4c954c
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-14 15:48:23 -0300

    #23 Modelo de Usuário

---
+++ backend/models/userModel.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please Enter your name"],
+      maxLength: [
+        25,
+        "Invalid name, Please enter a name with fewer than 25 characters",
+      ],
+      minLength: [3, "Name should contain more than 3 characters"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please Enter your email"],
+      unique: true,
+      validate: [validator.isEmail, "Please enter valid email"],
+    },
+    password: {
+      type: String,
+      required: [true, "Please Enter your password"],
+      minLength: [8, "Password should contain more than 8 characters"],
+      select: false,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema)
