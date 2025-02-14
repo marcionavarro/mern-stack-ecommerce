11b18276935dd50b7eccd36e4f11e18d6e9e7dad
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-14 18:09:58 -0300

    #28 Funcionalidade de login do usuário

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,26 +1,44 @@
-
 import handleAsyncError from "../middleware/handleAsyncError.js";
+import HandleError from "../utils/handleError.js";
 import User from "../models/userModel.js";
 
+export const registerUser = handleAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-export const registerUser = handleAsyncError( async(req, res, next) => {
-   const {name, email, password} = req.body;
-   
-   const user = await User.create({
-        name,
-        email,
-        password,
-        avatar: {
-            public_id: "This is temp id",
-            url: "This is temp url",
-        }
-   })
-   
-   const token = user.getJWTToken();
-   
-   res.status(201).json({
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "This is temp id",
+      url: "This is temp url",
+    },
+  });
+
+  const token = user.getJWTToken();
+
+  res.status(201).json({
+    success: true,
+    user,
+    token,
+  });
+});
+
+export const loginUser = handleAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    return next(new HandleError("Email or password cannot be empty", 400));
+  }
+  
+  const user = await User.findOne({email}).select("+password");
+  if(!user){
+    return next(new HandleError("Invalid Email or password", 401))
+  }
+  const token = user.getJWTToken();
+  res.status(200).json({
     success: true,
     user,
     token
-   })
-})
+  })
+});
