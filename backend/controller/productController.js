1a6bd5d6b5ee81cefcd83271b6d4f43c8f99a9a9
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-14 12:02:59 -0300

    #15 Erro de validação de backend

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -1,60 +1,60 @@
 import Product from "./../models/productModel.js";
-import HandleError from "./../utils/handleError.js"
-
+import HandleError from "./../utils/handleError.js";
+import handleAsyncError from "../middleware/handleAsyncError.js";
 
 // Creating Products
-export const createProducts = async (req, res) => {
+export const createProducts = handleAsyncError(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 
 // Get all Products
-export const getAllProducts = async (req, res) => {
+export const getAllProducts = handleAsyncError(async (req, res, next) => {
   const products = await Product.find();
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 // Update Product
-export const updateProduct = async (req, res, next) => {
-    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true
-    })
-    if(!product){
-        return next(new HandleError("Product Not Found", 404))
-    }
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
+export const updateProduct = handleAsyncError(async (req, res, next) => {
+  const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!product) {
+    return next(new HandleError("Product Not Found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
 
 // Delete Product
-export const deleteProduct = async (req, res, next) => {
-    const product = await Product.findByIdAndDelete(req.params.id)
-    if(!product){
-        return next(new HandleError("Product Not Found", 404))
-    }
-    res.status(200).json({
-        success: true,
-        message: "Product Deleted successfully"
-    })
-}
+export const deleteProduct = handleAsyncError(async (req, res, next) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+  if (!product) {
+    return next(new HandleError("Product Not Found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    message: "Product Deleted successfully",
+  });
+});
 
 // Accesse Single Product
-export const getSingleProduct = async (req, res, next) => {
-    let product = await Product.findById(req.params.id)
-    if(!product){
-        return next(new HandleError("Product Not Found", 404))
-    }
-    res.status(200).json({
-        success: true,
-        product
-    })
-}
+export const getSingleProduct = handleAsyncError(async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new HandleError("Product Not Found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+});
