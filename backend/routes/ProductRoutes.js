5b2c0c527a268005eac1762e9c79cd0be8b150b4
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-21 17:16:40 -0300

    #31 Verificando a autenticação do usuário

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,20 +1,23 @@
 import express from "express";
 import {
-  createProducts,
-  deleteProduct,
-  getAllProducts,
-  getSingleProduct,
-  updateProduct,
+    createProducts,
+    deleteProduct,
+    getAllProducts,
+    getSingleProduct,
+    updateProduct,
 } from "../controller/productController.js";
+import {verifyUserAuth} from "../middleware/userAuth.js";
 
 const router = express.Router();
 
 //Routes
-router.route("/products").get(getAllProducts).post(createProducts);
-router
-  .route("/product/:id")
-  .get(getSingleProduct)
-  .put(updateProduct)
-  .delete(deleteProduct);
+router.route("/products")
+    .get(verifyUserAuth, getAllProducts)
+    .post(createProducts);
+
+router.route("/product/:id")
+    .get(getSingleProduct)
+    .put(updateProduct)
+    .delete(deleteProduct);
 
 export default router;
