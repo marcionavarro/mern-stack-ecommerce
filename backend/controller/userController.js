3903e94da8830e4ac1b7ef8c8cb4ca7dd0063f24
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-21 16:20:55 -0300

    #30 Configurando cookies e código de refatoração

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
 import handleAsyncError from "../middleware/handleAsyncError.js";
 import HandleError from "../utils/handleError.js";
 import User from "../models/userModel.js";
+import { sendToken } from "../utils/jwtToken.js";
 
 export const registerUser = handleAsyncError(async (req, res, next) => {
   const { name, email, password } = req.body;
@@ -14,14 +15,7 @@ export const registerUser = handleAsyncError(async (req, res, next) => {
       url: "This is temp url",
     },
   });
-
-  const token = user.getJWTToken();
-
-  res.status(201).json({
-    success: true,
-    user,
-    token,
-  });
+  sendToken(user, 201, res)
 });
 
 export const loginUser = handleAsyncError(async (req, res, next) => {
@@ -40,11 +34,5 @@ export const loginUser = handleAsyncError(async (req, res, next) => {
   if(!isPasswordValid){
     return next(new HandleError("Invalid Email or password", 401))
   }
-
-  const token = user.getJWTToken();
-  res.status(200).json({
-    success: true,
-    user,
-    token
-  })
+  sendToken(user, 200, res)
 });
