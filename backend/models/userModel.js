3cc1bba5ab2e07419f5d0e12d64aca58ade3591c
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-24 17:38:03 -0300

    #35 Gerando token de reinicialização

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import validator from "validator";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import crypto from "crypto";
 
 const userSchema = new mongoose.Schema(
   {
@@ -63,6 +64,17 @@ userSchema.methods.getJWTToken = function () {
 
 userSchema.methods.verifyPassword = async function (userEnterPassword) {
   return await bcrypt.compare(userEnterPassword, this.password);
-}
+};
+
+// generating token
+userSchema.methods.generatePasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(20).toString("hex");
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.resetPasswordToken = Date.now() + 30 * 60 * 1000; //30minutes
+  return resetToken;
+};
 
 export default mongoose.model("User", userSchema);
