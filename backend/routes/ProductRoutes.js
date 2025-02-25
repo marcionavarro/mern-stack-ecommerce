5be016502b0ec7a8b62ad1aa4c03815cf88d8c06
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-02-25 16:05:55 -0300

    #45 Mudanças nas rotas de administração

--- backend/routes/ProductRoutes.js
+++ backend/routes/ProductRoutes.js
@@ -1,23 +1,24 @@
 import express from "express";
 import {
-    createProducts,
-    deleteProduct,
-    getAllProducts,
-    getSingleProduct,
-    updateProduct,
+  createProducts,
+  deleteProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
 } from "../controller/productController.js";
-import {roleBasedAccess, verifyUserAuth} from "../middleware/userAuth.js";
+import { roleBasedAccess, verifyUserAuth } from "../middleware/userAuth.js";
 
 const router = express.Router();
 
 //Routes
-router.route("/products")
-    .get(verifyUserAuth, getAllProducts)
-    .post(verifyUserAuth, roleBasedAccess("admin"), createProducts);
-
-router.route("/product/:id")
-    .get(verifyUserAuth, getSingleProduct)
-    .put(verifyUserAuth,  roleBasedAccess("admin"), updateProduct)
-    .delete(verifyUserAuth,  roleBasedAccess("admin"), deleteProduct);
+router.route("/products").get(verifyUserAuth, getAllProducts);
+router
+  .route("/admin/product/create")
+  .post(verifyUserAuth, roleBasedAccess("admin"), createProducts);
+router
+  .route("/admin/product/:id")
+  .put(verifyUserAuth, roleBasedAccess("admin"), updateProduct)
+  .delete(verifyUserAuth, roleBasedAccess("admin"), deleteProduct);
+router.route("/product/:id").get(getSingleProduct);
 
 export default router;
