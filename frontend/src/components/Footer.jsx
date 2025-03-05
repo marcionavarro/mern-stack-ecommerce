83e456e931bc15f8d3b18a0c04f403d813cca927
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-05 18:08:35 -0300

    #63 Rodapé

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Phone,
+  Mail,
+  GitHub,
+  LinkedIn,
+  YouTube,
+  Instagram,
+} from "@mui/icons-material";
+import "../componentStyles/Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+        {/* Section1 */}
+        <div className="footer-section contact">
+          <h3>Contact Us</h3>
+          <p>
+            <Phone fontSize="small" />
+            Phone: +9865467888
+          </p>
+          <p>
+            <Mail fontSize="small" />
+            Email: mnfullstack@gmail.com
+          </p>
+        </div>
+
+        {/* Section2 */}
+        <div className="footer-section social">
+          <h3>Follow me</h3>
+          <div className="social-links">
+            <a href="" target="_blank">
+              <GitHub className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <LinkedIn className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <YouTube className="social-icon" />
+            </a>
+            <a href="" target="_blank">
+              <Instagram className="social-icon" />
+            </a>
+          </div>
+        </div>
+
+        {/* Section3 */}
+        <div className="footer-section about">
+          <h3>About</h3>
+          <p>
+            Providing web development tutorials and courses to help you grow
+            your skills;
+          </p>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2025 Marcio Navarro. All rights reserved</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Footer from "../components/Footer";
+import "../pageStyles/Home.css";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className="home-container">
+        <h2 className="home-heading">Trending Now</h2>
+      </div>
+      <Footer />
+    </>
+  );
 }
 
-export default Home
+export default Home;
