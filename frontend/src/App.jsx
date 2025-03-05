f61bfc953acd87b699c867dd9633e4832e9fdbd2
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-04 21:00:46 -0300

    #62 Configuração de frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Home from './pages/Home';
+import { BrowserRouter as Router,  Routes, Route } from 'react-router-dom'
+
+
+export default function App() {
+  return (
+    <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
+  )
+}
