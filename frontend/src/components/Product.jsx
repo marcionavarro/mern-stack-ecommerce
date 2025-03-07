5d3ad575e008af3bf4f8b5aa1740f0ba14c8be74
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-07 20:48:50 -0300

    #66 Trabalhando em produtos

---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../componentStyles/Product.css";
+import { Link } from "react-router-dom";
+
+function Product({ product }) {
+  console.log(product);
+  return (
+    <Link to={product._id} className="product_id">
+      <div className="product-card">
+        <img src={product.image[0].url} alt={product.name} />
+        <div className="product-details">
+          <h3 className="product-title">{product.name}</h3>
+          <p className="product-price">
+            <strong>Price</strong> {product.price}/-
+          </p>
+          <button className="add-to-cart">Add to cart</button>
+        </div>
+      </div>
+    </Link>
+  );
+}
+
+export default Product;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,66 @@ import Footer from "../components/Footer";
 import "../pageStyles/Home.css";
 import Navbar from "../components/Navbar";
 import { ImageSlider } from "../components/ImageSlider";
+import Product from "../components/Product";
+
+const products = [
+  {
+    _id: "67af628161452d7d2bcbf86d",
+    name: "Product 2",
+    description: "Product description2",
+    price: 80,
+    ratings: 0,
+    image: [
+      {
+        public_id: "This is test id2",
+        url: "This is test rul2",
+        _id: "67af628161452d7d2bcbf86e",
+      },
+    ],
+    category: "phone",
+    stock: 0,
+    numOfReviews: 0,
+    reviews: [
+      {
+        user: "67bce99c4d54d700a19c4a6d",
+        name: "Fullstack Develloper",
+        rating: 3,
+        _id: "67bf735af6e030212c02fd9c",
+        comment: "Good",
+      },
+    ],
+    createdAt: "2025-02-14T15:34:25.406Z",
+    __v: 1,
+  },
+  {
+    _id: "67af62a461452d7d2bcbf873",
+    name: "Mobile",
+    description: "Product description3",
+    price: 1400,
+    ratings: 2,
+    image: [
+      {
+        public_id: "This is test id3",
+        url: "This is test rul3",
+        _id: "67af62a461452d7d2bcbf874",
+      },
+    ],
+    category: "mobile",
+    stock: 1,
+    numOfReviews: 1,
+    reviews: [
+      {
+        user: "67bce99c4d54d700a19c4a6d",
+        name: "Fullstack Develloper",
+        rating: 2,
+        comment: "Novo Teste",
+        _id: "67bf7699c5728e42c18df937",
+      },
+    ],
+    createdAt: "2025-02-14T15:35:00.519Z",
+    __v: 2,
+  },
+];
 
 function Home() {
   return (
@@ -11,6 +71,11 @@ function Home() {
       <ImageSlider />
       <div className="home-container">
         <h2 className="home-heading">Trending Now</h2>
+        <div className="home-product-container">
+          {products.map((product) => (
+            <Product  key={product.id} product={product} />
+          ))}
+        </div>
       </div>
       <Footer />
     </>
