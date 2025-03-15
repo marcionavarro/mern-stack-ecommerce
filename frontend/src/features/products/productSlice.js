21c838ddf4227bbf561f024529d6f5aa6edf12d0
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-15 14:58:17 -0300

    #70 Buscar dados do produto usando API - createAsyncThunk

--- frontend/src/features/products/productSlice.js
+++ frontend/src/features/products/productSlice.js
@@ -1,4 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const getProduct = createAsyncThunk(
+  "product/getProduct",
+  async (_, rejectWithValue) => {
+    try {
+      const link = "/api/v1/products";
+      const data = await axios.get(link);
+      console.log("Response", data);
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "An error occurred");
+    }
+  }
+);
 
 const productSlice = createSlice({
   name: "product",
@@ -12,6 +26,24 @@ const productSlice = createSlice({
     removeErrors: (state) => {
       state.error = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getProduct.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProduct.fulfilled, (state, action) => {
+        console.log("Fulfilled action payload", action.payload);
+        state.loading = false;
+        state.error = null;
+        state.products = action.payload.products;
+        state.productCount = action.payload.productCount
+      })
+      .addCase(getProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || 'Somthing went wrong';
+      });
   },
 });
 
