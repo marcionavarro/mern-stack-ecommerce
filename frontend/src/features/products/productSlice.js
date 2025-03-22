2cbd1f06d817f4df2c55315a879fadbebd4d1602
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-22 10:28:08 -0300

    #75 Obter detalhes de um único produto

--- frontend/src/features/products/productSlice.js
+++ frontend/src/features/products/productSlice.js
@@ -3,9 +3,24 @@ import axios from "axios";
 
 export const getProduct = createAsyncThunk(
   "product/getProduct",
-  async (_, {rejectWithValue}) => {
+  async (_, { rejectWithValue }) => {
     try {
       const link = "/api/v1/products";
+      const { data } = await axios.get(link);
+      console.log("Response", data);
+      return data;
+    } catch (error) {
+      return rejectWithValue(error.response?.data || "An error occurred");
+    }
+  }
+);
+
+// Product Details
+export const getProductDetails = createAsyncThunk(
+  "product/getProductDetails",
+  async (id, { rejectWithValue }) => {
+    try {
+      const link = `/api/v1/product/${id}`;
       const { data } = await axios.get(link);
       console.log("Response", data);
       return data;
@@ -22,6 +37,7 @@ const productSlice = createSlice({
     productCount: 0,
     loading: false,
     error: null,
+    product: null,
   },
   reducers: {
     removeErrors: (state) => {
@@ -42,6 +58,22 @@ const productSlice = createSlice({
         state.productCount = action.payload.productCount;
       })
       .addCase(getProduct.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload || "Something went wrong";
+      });
+
+    builder
+      .addCase(getProductDetails.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getProductDetails.fulfilled, (state, action) => {
+        console.log("Product Details action payload", action.payload);
+        state.loading = false;
+        state.error = null;
+        state.products = action.payload.product;
+      })
+      .addCase(getProductDetails.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload || "Something went wrong";
       });
