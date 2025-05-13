5a422377ed389f2bef98dc26da9c8eb8225353de
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-13 19:33:41 -0300

    #88 Formulário de registro | Manipulador de eventos ...

--- frontend/src/User/Register.jsx
+++ frontend/src/User/Register.jsx
@@ -3,28 +3,63 @@ import "../UserStyles/Form.css";
 import { Link } from "react-router-dom";
 
 function Register() {
-    const [user, setUser] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
-    const [avatar, setAvatar] = useState('');
-    const [avatarPreview, setAvatarPreview] = useState('/images/profile.png');
-    const {name, email, password} = user;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const [avatar, setAvatar] = useState("");
+  const [avatarPreview, setAvatarPreview] = useState("/images/profile.png");
+  const { name, email, password } = user;
+
+  const registerDataChange = (e) => {
+    if (e.target.name === "avatar") {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (reader.readyState === 2) {
+          setAvatarPreview(reader.result);
+          setAvatar(reader.result);
+        }
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
+    }
+  };
 
   return (
-    <div className="form-container"> {/* container */}
+    <div className="form-container">
+      {" "}
+      {/* container */}
       <div className="form-content">
         <form className="form">
           <h2>Sign Up</h2>
           <div className="input-group">
-            <input type="text" placeholder="Username" name="name" value={name} />
+            <input
+              type="text"
+              placeholder="Username"
+              name="name"
+              value={name}
+              onChange={registerDataChange}
+            />
           </div>
           <div className="input-group">
-            <input type="email" placeholder="Email" name="email" value={email}/>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              value={email}
+              onChange={registerDataChange}
+            />
           </div>
           <div className="input-group">
-            <input type="password" placeholder="Password" name="password" value={password}/>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={password}
+              onChange={registerDataChange}
+            />
           </div>
           <div className="input-group avatar-group">
             <input
@@ -32,6 +67,7 @@ function Register() {
               name="avatar"
               className="file-input"
               accept="image/"
+              onChange={registerDataChange}
             />
             <img src={avatarPreview} alt="Avatar Preview" className="avatar" />
           </div>
