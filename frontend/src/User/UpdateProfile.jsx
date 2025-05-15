109a53e3d9acf08958e4a0a0cb7c80794ea3cb26
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-15 18:39:00 -0300

    #106 trabalhando na atualização da imagem do perfil

--- frontend/src/User/UpdateProfile.jsx
+++ frontend/src/User/UpdateProfile.jsx
@@ -2,13 +2,27 @@ import React, { useState } from "react";
 import "../UserStyles/Form.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { toast } from "react-toastify";
 
 function UpdateProfile() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [avatar, setAvatar] = useState("");
   const [avatarPreview, setAvatarPreview] = useState("./images/profile.png");
-  
+
+  const profileImageUpdate = (e) => {
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setAvatarPreview(reader.result);
+        setAvatar(reader.result);
+      }
+    };
+    reader.onerror = (error) => {
+      toast.error("Error reading file: ", error);
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   return (
     <>
@@ -18,14 +32,27 @@ function UpdateProfile() {
           <form className="form">
             <h2>Update Profile</h2>
             <div className="input-group avatar-group">
-              <input type="file" accept="image/" className="file-input" />
+              <input
+                type="file"
+                accept="image/"
+                className="file-input"
+                onChange={profileImageUpdate}
+              />
               <img src={avatarPreview} alt="User profile" className="avatar" />
             </div>
             <div className="input-group">
-              <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="input-group">
-              <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <button className="authBtn">Upadte</button>
           </form>
