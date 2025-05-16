2b5ac8dbd883c0f1b2a3eea7c6efaccbfe9c461f
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-16 19:36:57 -0300

    #119 Ação de redefinição de senha de despacho

--- frontend/src/User/ResetPassword.jsx
+++ frontend/src/User/ResetPassword.jsx
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageTitle from "../components/PageTitle";
 import "../UserStyles/Form.css";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  removeErrors,
+  removeSuccess,
+  resetPassword,
+} from "../features/user/userSlice";
+import { toast } from "react-toastify";
 
 function ResetPassword() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  console.log(useParams())
+  const { success, loading, error } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { token } = useParams();
 
   const resetPasswordSubmit = (e) => {
     e.preventDefault();
@@ -14,8 +24,26 @@ function ResetPassword() {
       password,
       confirmPassword,
     };
-    console.log(data);
+    dispatch(resetPassword({ token, userData: data }));
   };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error, { position: "top-center", autoClose: 3000 });
+      dispatch(removeErrors());
+    }
+  }, [dispatch, error]);
+
+  useEffect(() => {
+    if (success) {
+      toast.success("Password Reset succesfully", {
+        position: "top-center",
+        autoClose: 3000,
+      });
+      dispatch(removeSuccess());
+      navigate("/login");
+    }
+  }, [dispatch, success]);
 
   return (
     <>
