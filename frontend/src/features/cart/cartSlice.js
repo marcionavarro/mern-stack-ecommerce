1e1cbab9c11e5245917513e663d8d5aec0e6f222
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-19 15:41:59 -0300

    #123 Verifique se há itens existentes no carrinho an...

--- frontend/src/features/cart/cartSlice.js
+++ frontend/src/features/cart/cartSlice.js
@@ -25,7 +25,7 @@ export const addItemsToCart = createAsyncThunk(
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    cartItems: [],
+    cartItems: JSON.parse(localStorage.getItem("cartItems")) || [],
     loading: false,
     error: null,
     success: false,
@@ -48,11 +48,23 @@ const cartSlice = createSlice({
       })
       .addCase(addItemsToCart.fulfilled, (state, action) => {
         const item = action.payload;
-        state.cartItems.push(item);
+
+        const existingItem = state.cartItems.find(
+          (i) => i.product === item.product
+        );
+
+        if (existingItem) {
+          existingItem.quantity = item.quantity;
+          state.message = `Updated ${item.name} quantity to in the cart}`;
+        } else {
+          state.cartItems.push(item);
+          state.message = `${item.name} is added to cart successfully`;
+        }
+
         state.loading = false;
         state.error = null;
         state.success = true;
-        state.message = `${item.name} is added to cart successfully`;
+        localStorage.setItem("cartItems", JSON.stringify(state.cartItems));
       })
       .addCase(addItemsToCart.rejected, (state) => {
         state.loading = false;
