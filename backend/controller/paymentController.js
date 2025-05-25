edc735e1ad1ea55767ebab4db96565c2bbb8fdca
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-25 14:32:06 -0300

    #140 Envio da chave de API Strip para o frontend | Configu...

--- backend/controller/paymentController.js
+++ backend/controller/paymentController.js
@@ -1,16 +1,22 @@
 import { stripe } from "../utils/stripe.js";
 import handleAsyncError from "../middleware/handleAsyncError.js";
 
-
 export const processPayment = handleAsyncError(async (req, res) => {
-    const options = {
-        amount: Number(req.body.amount * 100),
-        currency: 'BRL'
-    }
+  const options = {
+    amount: Number(req.body.amount * 100),
+    currency: "BRL",
+  };
 
-    const order = await stripe.paymentIntents.create(options);
-    res.status(200).json({
-        success: true,
-        order
-    })
-})
+  const order = await stripe.paymentIntents.create(options);
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// Sen API Key
+export const sendAPIKey = handleAsyncError(async (req, res) => {
+  res.status(200).json({
+    key: process.env.STRIPE_SECRET,
+  });
+});
