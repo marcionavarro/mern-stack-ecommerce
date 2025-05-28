33327d33071efd3d657d8d72069186906118d43d
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-05-28 00:29:35 -0300

    #149 trabalhando na tabela de pedidos do usuário

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,6 +19,7 @@ import Shipping from "./Cart/Shipping";
 import OrderConfirm from "./Cart/OrderConfirm";
 import Payment from "./Cart/Payment";
 import PaymentSuccess from "./Cart/PaymentSuccess";
+import MyOrders from "./Orders/MyOrders";
 
 export default function App() {
   const { isAuthenticated, user } = useSelector((state) => state.user);
@@ -71,6 +72,10 @@ export default function App() {
         <Route
           path="/paymentSuccess"
           element={<ProtectedRoute element={<PaymentSuccess />} />}
+        />
+        <Route
+          path="orders/user"
+          element={<ProtectedRoute element={<MyOrders />} />}
         />
       </Routes>
       {isAuthenticated && <UserDashboard user={user} />}
